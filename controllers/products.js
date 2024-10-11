e62afdce12212f0978c34d114cc8bea70052fdaa
BluePrisoner <dragonemperor9868@gmail.com>
2024-10-12 00:08:25 +0530

    Search feature - 1

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,31 @@
+const Products = require('../models/product') 
+
 
 const getAllProductStatic = async (req,res) => {
-    throw new Error('Static Error')
-    res.status(200).json({msg: 'testing route'});
+     const products  = await Products.find({
+        featured: true
+    })
+    res.status(200).json({products,nbHits: products.length});
 }
 const getAllProduct = async (req,res) => {
-    res.status(200).json({msg: 'products route'});
+
+    const {featured, company,name,rating} = req.query;
+    const queryObj = {};
+    if(featured){
+        queryObj.featured = featured === 'true'? true : false
+    }
+    if(company){
+        queryObj.company = company;
+    }
+    if(name){
+        queryObj.name = {$regex: name, $options: 'i'};  //option i is for case-insensitive
+    }
+    if(rating){
+        queryObj.rating = {$gt: Number(rating)}
+    }
+    console.log(queryObj)
+    const products = await Products.find(queryObj)
+    res.status(200).json({products, nbHits: products.length});
 }
 
 module.exports = {getAllProduct,getAllProductStatic}
--- models/product.js
+++ models/product.js
@@ -17,6 +17,10 @@ const productSchema  = new mongoose.Schema({
         type: Date,
         default: Date.now(),
     },
+    featured: {
+        type: Boolean,
+        default: false,
+    },
     company: {
         type: String,
         enum: {
